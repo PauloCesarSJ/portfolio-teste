09ba2d6420e0aa94034f462a4705f9312a740ea0
paulo200455 <pcesar.sj2004@gmail.com>
2023-02-03 20:23:21 -0300

    criação de home e seus módulos

--- portfolio/src/componentes/paginas/home/index.tsx
+++ portfolio/src/componentes/paginas/home/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import Menu from '../../menu';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 
 function Home() {
 
     return (
-        <div>
+        <div >
             <Menu/>
-            <h1>Sou Paulo sou um desenvolver front end</h1>
+            <div className='Home'>
+                <div id='HomeSaudacao'>
+                    <h2>OLA ! SOU O <samp>PAULO</samp>. </h2>
+                    <h1>
+                        Sou fissurado em jogos de RPG, e de detetive, amo ter a sensação de resolver problemas complexos,
+                        atualmente estou fazendo faculdade de ciências da computação.
+                    </h1>
+                </div>
+                <div id='HomePort'>
+                    <h2>alguns trabalhos</h2>
+                    <img src="img/construcao.png" alt="Em construção" id='' /> 
+                    <img src="img/construcao.png" alt="Em construção" id='' /> 
+                    <img src="img/construcao.png" alt="Em construção" id='' /> 
+                </div>
+                <div id='HomeEu'>
 
-
+                </div>
+            </div>
+           
         </div>
     )
 }
