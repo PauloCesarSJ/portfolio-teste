101da24d40c41278edb6a5a22cab0239ad5f7d49
paulo200455 <pcesar.sj2004@gmail.com>
2023-02-04 19:11:05 -0300

    home eu

--- portfolio/src/componentes/paginas/home/index.tsx
+++ portfolio/src/componentes/paginas/home/index.tsx
@@ -16,13 +16,34 @@ function Home() {
                     </h1>
                 </div>
                 <div id='HomePort'>
-                    <h2>alguns trabalhos</h2>
-                    <img src="img/construcao.png" alt="Em construção" id='' /> 
-                    <img src="img/construcao.png" alt="Em construção" id='' /> 
-                    <img src="img/construcao.png" alt="Em construção" id='' /> 
+                    <div>
+                        <img src="img/construcao.png" alt="Em construção" id='imgMenuPort1' /> 
+                        <img src="img/construcao.png" alt="Em construção" id='imgMenuPort2' /> 
+                        <img src="img/construcao.png" alt="Em construção" id='imgMenuPort3' /> 
+                        <img src="img/construcao.png" alt="Em construção" id='imgMenuPort4' /> 
+                    </div>
+                    <button>MAIS</button>
                 </div>
                 <div id='HomeEu'>
-
+                    <div>
+                        <p>     
+                            Ola eu sou Paulo Cesar, tenho 18 anos, sou um jovem, web developer júnior.
+                            Sou apaixonado por tecnologia. Minha história com tecnologia começa aos meus 14 anos
+                            onde meu professo de física me apresentando um Arduíno, usando c++ para fazer o básico,
+                            ali eu me apaixonei pela área da tecnologia, passei alguns anos pulando de língua em língua
+                            ate encontrar uma que permitisse a junção de tecnologia e artes foi aí que html e css apareceu na minha
+                            vida, para ser mais preciso durante meu primeiro período na faculdade de matemática, nesse momento
+                            eu percebi que matemática por si não seria mais o suficiente então decidir mudar, sair de matemática
+                            e mudar para um curso na área de tecnologias.
+                        </p>
+                        <p>
+                            Meus conhecimentos na área de desenvolvimento web são  html,
+                            css, javascript,Typescript ,react, sass, git e alguns conhecimentos teóricos, conhecimento básico
+                            de edição de imagens e vídeos também podem ser considerados, e o domínio da
+                            língua inglesa, poder ser melhor vistos na área do meus projetos.
+                        </p>
+                    </div>
+                    <img src="img/eu.png" alt="" />
                 </div>
             </div>
            
