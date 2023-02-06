450829eb3d62a7994e83112b996eacc29fd62808
PauloCesarSJ <pcesar.sj2004@gmail.com>
2023-02-06 18:24:29 -0300

    .

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './componentes/paginas/home';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router'
 import Wiki from './componentes/paginas/WikimediaApi/WikimediaApi';
 
 function App() {
